9cc3b1741a388dcbfd6b92ae1d7f14de5c23a0cf
julianavalenti <julianabmartinez2@icloud.com>
2023-04-10 15:01:19 -0400

    adds completed CRUD

--- controllers/sessions.js
+++ controllers/sessions.js
@@ -19,6 +19,48 @@ sessionsRouter.get('/logout', (req, res) => {
     });
 });
 
+//U
+
+sessionsRouter.put('/edit', async (req, res) => {
+    try {
+        const foundUser = await User.findById(req.session.currentUser._id);
+
+        foundUser.name = req.body.name;
+        foundUser.email = req.body.email;
+        foundUser.phone = req.body.phone;
+
+        await foundUser.save();
+        req.session.currentUser = foundUser
+        res.redirect('/sessions/account');
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error updating personal information');
+    }
+});
+
+sessionsRouter.put('/password', async (req, res) => {
+    try {
+        const foundUser = await User.findById(req.session.currentUser._id);
+
+        
+        if (req.body.newPassword) {
+            const newPassword = await bcrypt.hash(req.body.newPassword, 10);
+            foundUser.password = newPassword;
+            await foundUser.save();
+            res.redirect('/sessions/account');
+        } else {
+            
+            res.status(400).send('New password is required');
+        }
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error updating password');
+    }
+   
+});
+
 //C
 
 sessionsRouter.post('/login', async (req, res) => {
@@ -44,5 +86,24 @@ sessionsRouter.post('/login', async (req, res) => {
       res.status(500).send('Error logging in user');
     }
   });
+
+  //E
+
+  sessionsRouter.get("/edit", async (req,res) => {
+    const foundUser = await User.findById(
+        req.params.id,
+        );
+    res.render("session/edit.ejs", {
+        user: foundUser,
+    })
+})
+  //S 
+
+  sessionsRouter.get('/account', (req, res) => {
+	
+    res.render('session/account.ejs', {
+        
+    });
+}); 
 
 module.exports = sessionsRouter;
